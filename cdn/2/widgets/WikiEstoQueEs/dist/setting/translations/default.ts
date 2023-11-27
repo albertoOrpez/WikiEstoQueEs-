e0386c9b2de3fab5d59551fe299ff1f63745d071
Alberto Orpez Milan <alberto.orpezmilan@gmail.com>
2023-11-27 17:53:45 +0100

    Add app

---
+++ cdn/2/widgets/WikiEstoQueEs/dist/setting/translations/default.ts
@@ -0,0 +1,20 @@
+export default {
+  selectMapWidget: 'Select Map Widget',
+  symbolSettings: 'Symbol Settings',
+  zoomSettings: 'Zoom Settings',
+  debugSettings: 'Debug Settings',
+  debugEnabled: 'Debug Enabled',
+  icon2D: 'Icon 2D',
+  icon3D: 'Icon 3D',
+  iconSymbol2DSize: 'Icon Symbol 2D Size (in pixels)',
+  iconSymbol3DSize: 'Icon Symbol 3D Size (in pixels)',
+  objectSymbol3DSize: 'Object Symbol 3D Size (in meters)',
+  icon3DColor: 'Icon3D Color',
+  minWorldLength: 'Minimum 3D Vertical symbol offset (in world units)',
+  maxWorldLength: 'Maximum 3D Vertical symbol offset (in world units)',
+  screenLength: '3D Vertical symbol offset (in screen size)',
+  lineCallout3DColor: 'Line Callout 3D Color',
+  lineCallout3DSize: 'Line Callout 3D Size',
+  defaultZoomLevel: 'Default Zoom Level',
+  defaultTilt: 'Default Tilt'
+}
